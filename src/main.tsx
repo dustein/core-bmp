323f2c9a2d3a8286b077ad25907b95cb7ab488f9
dustein <steindu@gmail.com>
2025-12-28 20:45:03 -0300

    speedinsight vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SpeedInsights/>
     <App />
   </StrictMode>,
 )
