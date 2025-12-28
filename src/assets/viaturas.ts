4927b33e6dcf39e495c3f4d738658d6b5feedd95
dustein <steindu@gmail.com>
2025-12-28 10:50:54 -0300

    listas atualizadas

--- src/assets/viaturas.ts
+++ src/assets/viaturas.ts
@@ -1,16 +1,25 @@
 // src/assets/viaturas.ts
 
 export const LISTA_VIATURAS = [
-  { prefixo: "61-1234", placaOficial: "LCL-1234", placaReservada: "KXP-5021" },
-  { prefixo: "61-5678", placaOficial: "LCL-5678", placaReservada: "KOW-9988" },
-  { prefixo: "61-9000", placaOficial: "LCL-9000", placaReservada: "KPR-1212" },
-  { prefixo: "61-1111", placaOficial: "LCL-1111", placaReservada: "KOW-9988" },
-  { prefixo: "61-2222", placaOficial: "LCL-2222", placaReservada: "KOW-9988" },
-  { prefixo: "61-3333", placaOficial: "LCL-3333", placaReservada: "KOW-9988" },
-  { prefixo: "61-4444", placaOficial: "LCL-4444", placaReservada: "KOW-9988" },
-  { prefixo: "61-5555", placaOficial: "LCL-5555", placaReservada: "KOW-9988" },
-  { prefixo: "61-6666", placaOficial: "LCL-6666", placaReservada: "KOW-9988" },
-  { prefixo: "61-7777", placaOficial: "LCL-7777", placaReservada: "KOW-9988" },
-  { prefixo: "61-8888", placaOficial: "LCL-8888", placaReservada: "KOW-9988" },
-  // Adicione outras viaturas conforme necessário
+  { prefixo: "11-0497", placaOficial: "RKK-4G47", placaReservada: "" },
+  { prefixo: "11-0498", placaOficial: "RKK-4G48", placaReservada: "" },
+  { prefixo: "11-0499", placaOficial: "RKK-4G49", placaReservada: "" },
+  { prefixo: "11-0500", placaOficial: "RKL-4H02", placaReservada: "" },
+  { prefixo: "11-0501", placaOficial: "RKK-4G50", placaReservada: "" },
+  { prefixo: "11-0502", placaOficial: "RKL-4G91", placaReservada: "" },
+  { prefixo: "11-0503", placaOficial: "RKL-4G92", placaReservada: "" },
+  { prefixo: "11-0504", placaOficial: "RKL-4G93", placaReservada: "" },
+  { prefixo: "11-0505", placaOficial: "RKL-4G98", placaReservada: "" },
+  { prefixo: "11-0506", placaOficial: "RKL-4G99", placaReservada: "" },
+  { prefixo: "11-0507", placaOficial: "RKL-4H01", placaReservada: "" },
+  { prefixo: "11-0508", placaOficial: "RKL-4G94", placaReservada: "" },
+  { prefixo: "11-0509", placaOficial: "RKL-4G95", placaReservada: "" },
+  { prefixo: "11-0510", placaOficial: "RKL-4G96", placaReservada: "" },
+  { prefixo: "11-0511", placaOficial: "RKL-4G97", placaReservada: "" },
+  { prefixo: "11-0512", placaOficial: "RKL-4H03", placaReservada: "" },
+  { prefixo: "11-0513", placaOficial: "RKK-4G45", placaReservada: "" },
+  { prefixo: "11-0514", placaOficial: "RKK-4G46", placaReservada: "" },
+  { prefixo: "11-0515", placaOficial: "RKK-4G51", placaReservada: "" },
+  { prefixo: "11-0516", placaOficial: "RKL-3G99", placaReservada: "" },
+  { prefixo: "11-0517", placaOficial: "RKL-4H00", placaReservada: "" }
 ];
